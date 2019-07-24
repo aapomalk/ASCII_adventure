6f250e9e7f94cf825e64c9b5197de13677d58060
Aapo Malkamäki <aapo.malkamaki@ssf.fi>
2019-07-24 16:53:42 +0300

    one more find function

--- src/Find.cpp
+++ src/Find.cpp
@@ -1,15 +1,33 @@
 
 #include "Find.h"
 
-// position find(const Rubic &cube, Side color, int xd = 1, int yd = 1);
+position find(const Rubic &cube, Side color, int xd, int yd) {
+  for (int x : {xd, cube.get_size()-1-xd}) {
+    for (int y : {yd, cube.get_size()-1-yd}) {
+      for (int i=0; i<num_of_sides; i++) {
+	Side current = static_cast<Side>(i);
+	if (cube.piece(current, x, y) == color) {
+	  return {current, x, y};
+	}
+	if (x == y) {
+	  continue;
+	}
+	if (cube.piece(current, y, x) == color) {
+	  return {current, y, x};
+	}
+      }
+    }
+  }
+  throw 0; // this should never be reached
+}
 // position find(const Rubic &cube, Side color1, Side color2, int zd = 1);
 // position find(const Rubic &cube, Side color1, Side color2, Side color3);
 
 Side find_any(const Rubic &cube, Side color) {
-  for (int i=0; i<num_of_sides; i++) {
-    Side current = static_cast<Side>(i);
-    for (int j=0; j<cube.get_size(); j++) {
-      for (int k=0; k<cube.get_size(); k++) {
+  for (int j=0; j<cube.get_size(); j++) {
+    for (int k=0; k<cube.get_size(); k++) {
+      for (int i=0; i<num_of_sides; i++) {
+	Side current = static_cast<Side>(i);
 	if (cube.piece(current, k, j) == color) {
 	  return current;
 	}
